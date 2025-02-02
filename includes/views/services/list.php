60cfbfbcad84813623d410425f2e9293e584018c
akradimo <tepars.ir@gmail.com>
2025-02-03 00:32:22 +0330

    Auto-deploy changes

--- includes/views/services/list.php
+++ includes/views/services/list.php
@@ -1,8 +1,8 @@
 <div class="wrap hesabfa-container">
     <h1>لیست خدمات</h1>
-    <a href="<?php echo admin_url('admin.php?page=hesabfa-services&action=add'); ?>" class="button button-primary">افزودن خدمت جدید</a>
-    <table class="hesabfa-table">
-        <thead>
+    <a href="<?php echo admin_url('admin.php?page=hesabfa-services&action=add'); ?>" class="btn btn-primary mb-3">افزودن خدمت جدید</a>
+    <table class="table table-bordered">
+        <thead class="table-dark">
         <tr>
             <th>نام خدمت</th>
             <th>کد خدمت</th>
@@ -19,8 +19,8 @@
                 <td><?php echo number_format($service->price, 2); ?></td>
                 <td><?php echo esc_html($service->description); ?></td>
                 <td>
-                    <a href="<?php echo admin_url('admin.php?page=hesabfa-services&action=edit&id=' . $service->id); ?>" class="button">ویرایش</a>
-                    <a href="<?php echo wp_nonce_url(admin_url('admin.php?page=hesabfa-services&action=delete&id=' . $service->id), 'hesabfa_delete_service_' . $service->id); ?>" class="button button-danger" onclick="return confirm('آیا مطمئن هستید؟');">حذف</a>
+                    <a href="<?php echo admin_url('admin.php?page=hesabfa-services&action=edit&id=' . $service->id); ?>" class="btn btn-sm btn-warning">ویرایش</a>
+                    <a href="<?php echo wp_nonce_url(admin_url('admin.php?page=hesabfa-services&action=delete&id=' . $service->id), 'hesabfa_delete_service_' . $service->id); ?>" class="btn btn-sm btn-danger" onclick="return confirm('آیا مطمئن هستید؟');">حذف</a>
                 </td>
             </tr>
         <?php endforeach; ?>
