040d0956d031aecc0afe653028f4e3f54594669b
akradimo <tepars.ir@gmail.com>
2025-02-03 00:34:26 +0330

    Auto-deploy changes

--- includes/views/invoices/list.php
+++ includes/views/invoices/list.php
@@ -1,8 +1,8 @@
 <div class="wrap hesabfa-container">
     <h1>لیست فاکتورها</h1>
-    <a href="<?php echo admin_url('admin.php?page=hesabfa-invoices&action=add'); ?>" class="button button-primary">افزودن فاکتور جدید</a>
-    <table class="hesabfa-table">
-        <thead>
+    <a href="<?php echo admin_url('admin.php?page=hesabfa-invoices&action=add'); ?>" class="btn btn-primary mb-3">افزودن فاکتور جدید</a>
+    <table class="table table-bordered">
+        <thead class="table-dark">
         <tr>
             <th>نام مشتری</th>
             <th>مبلغ کل</th>
@@ -17,8 +17,8 @@
                 <td><?php echo number_format($invoice->total_amount, 2); ?></td>
                 <td><?php echo esc_html($invoice->created_at); ?></td>
                 <td>
-                    <a href="<?php echo admin_url('admin.php?page=hesabfa-invoices&action=edit&id=' . $invoice->id); ?>" class="button">ویرایش</a>
-                    <a href="<?php echo wp_nonce_url(admin_url('admin.php?page=hesabfa-invoices&action=delete&id=' . $invoice->id), 'hesabfa_delete_invoice_' . $invoice->id); ?>" class="button button-danger" onclick="return confirm('آیا مطمئن هستید؟');">حذف</a>
+                    <a href="<?php echo admin_url('admin.php?page=hesabfa-invoices&action=edit&id=' . $invoice->id); ?>" class="btn btn-sm btn-warning">ویرایش</a>
+                    <a href="<?php echo wp_nonce_url(admin_url('admin.php?page=hesabfa-invoices&action=delete&id=' . $invoice->id), 'hesabfa_delete_invoice_' . $invoice->id); ?>" class="btn btn-sm btn-danger" onclick="return confirm('آیا مطمئن هستید؟');">حذف</a>
                 </td>
             </tr>
         <?php endforeach; ?>
