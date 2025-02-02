b923cbff4c493b879f90809b003c96fc8c67c572
akradimo <tepars.ir@gmail.com>
2025-02-02 10:51:29 +0330

    Auto-deploy changes

--- includes/core/Core.php
+++ includes/core/Core.php
@@ -31,19 +31,25 @@ class Core {
         );
     }
 
-    public static function enqueue_scripts() {
-        wp_enqueue_style('hesabfa-styles', HESABFA_PLUGIN_URL . 'assets/css/styles.css');
-        wp_enqueue_script('hesabfa-scripts', HESABFA_PLUGIN_URL . 'assets/js/scripts.js', ['jquery'], null, true);
-    }
+    public static function handle_products_page() {
+        $controller = new \Hesabfa\Controllers\ProductController();
 
-    public static function render_main_page() {
-        echo '<h1 class="welcome-title">خوش آمدید به حسابفا</h1>';
-        echo '<br>';
-        echo '<h1> برنامه حسابداری پیشرفته پارس تک ';
+        $action = isset($_GET['action']) ? sanitize_text_field($_GET['action']) : 'list';
+        $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
-    }
-
-    public static function render_products_page() {
-        include HESABFA_PLUGIN_DIR . 'includes/views/products/list.php';
-    }
+        switch ($action) {
+            case 'add':
+                $controller->add();
+                break;
+            case 'edit':
+                $controller->edit($id);
+                break;
+            case 'delete':
+                $controller->delete($id);
+                break;
+            default:
+                $controller->list();
+                break;
+        }
+        }
 }
