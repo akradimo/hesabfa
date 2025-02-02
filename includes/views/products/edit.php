5294485a5815b812446255caf2da0cb2ff7fea23
akradimo <tepars.ir@gmail.com>
2025-02-02 22:18:01 +0330

    Auto-deploy changes

--- includes/views/products/edit.php
+++ includes/views/products/edit.php
@@ -2,13 +2,24 @@
     <h1>ویرایش محصول</h1>
     <form method="post" action="">
         <?php wp_nonce_field('hesabfa_edit_product', 'hesabfa_edit_product_nonce'); ?>
-        <div class="mb-3">
-            <label for="name" class="form-label">نام محصول</label>
-            <input type="text" class="form-control" id="name" name="name" value="<?php echo esc_attr($product->name); ?>" required>
-        </div>
-        <div class="mb-3">
-            <label for="code" class="form-label">کد محصول</label>
-            <input type="text" class="form-control" id="code" name="code" value="<?php echo esc_attr($product->code); ?>" required>
-        </div>
-        <div class="mb-3">
-            <label for="price" class="form-label">قیمت</label
+        <table class="form-table">
+            <tr>
+                <th scope="row"><label for="name">نام محصول</label></th>
+                <td><input name="name" type="text" id="name" class="regular-text" value="<?php echo esc_attr($product->name); ?>" required></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="code">کد محصول</label></th>
+                <td><input name="code" type="text" id="code" class="regular-text" value="<?php echo esc_attr($product->code); ?>" required></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="price">قیمت</label></th>
+                <td><input name="price" type="number" id="price" step="0.01" value="<?php echo esc_attr($product->price); ?>" required></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="stock">موجودی</label></th>
+                <td><input name="stock" type="number" id="stock" value="<?php echo esc_attr($product->stock); ?>" required></td>
+            </tr>
+        </table>
+        <?php submit_button('ذخیره تغییرات'); ?>
+    </form>
+</div>
