5688cd56ce8a59277fdb959565b90127b2e8a9fd
akradimo <tepars.ir@gmail.com>
2025-02-02 10:00:11 +0330

    Auto-deploy changes

--- includes/core/Core.php
+++ includes/core/Core.php
@@ -38,6 +38,7 @@ class Core {
 
     public static function render_main_page() {
         echo '<h1>خوش آمدید به حسابفا</h1>';
+        echo '<br'
     }
 
     public static function render_products_page() {
