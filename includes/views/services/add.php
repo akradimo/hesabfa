89991790ab487899f9f4d7a9e9407a55238aa8e0
akradimo <tepars.ir@gmail.com>
2025-02-03 00:32:43 +0330

    Auto-deploy changes

--- includes/views/services/add.php
+++ includes/views/services/add.php
@@ -2,24 +2,22 @@
     <h1>افزودن خدمت جدید</h1>
     <form method="post" action="">
         <?php wp_nonce_field('hesabfa_add_service', 'hesabfa_add_service_nonce'); ?>
-        <table class="form-table">
-            <tr>
-                <th scope="row"><label for="name">نام خدمت</label></th>
-                <td><input name="name" type="text" id="name" class="regular-text" required></td>
-            </tr>
-            <tr>
-                <th scope="row"><label for="code">کد خدمت</label></th>
-                <td><input name="code" type="text" id="code" class="regular-text" required></td>
-            </tr>
-            <tr>
-                <th scope="row"><label for="price">قیمت</label></th>
-                <td><input name="price" type="number" id="price" step="0.01" required></td>
-            </tr>
-            <tr>
-                <th scope="row"><label for="description">توضیحات</label></th>
-                <td><textarea name="description" id="description" class="regular-text"></textarea></td>
-            </tr>
-        </table>
-        <?php submit_button('افزودن خدمت'); ?>
+        <div class="mb-3">
+            <label for="name" class="form-label">نام خدمت</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="mb-3">
+            <label for="code" class="form-label">کد خدمت</label>
+            <input type="text" class="form-control" id="code" name="code" required>
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">قیمت</label>
+            <input type="number" class="form-control" id="price" name="price" step="0.01" required>
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">توضیحات</label>
+            <textarea class="form-control" id="description" name="description"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">افزودن خدمت</button>
     </form>
 </div>
