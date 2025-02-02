dcd38efc3cacd79aabb841cd0fcbcb32daa40469
akradimo <tepars.ir@gmail.com>
2025-02-02 09:48:32 +0330

    Auto-deploy changes

--- includes/views/products/list.php
+++ includes/views/products/list.php
@@ -1,1 +1,18 @@
-ECHO is off.
+<div class="wrap hesabfa-container">
+    <h1>لیست محصولات</h1>
+    <a href="<?php echo admin_url('admin.php?page=hesabfa-products&action=add'); ?>" class="button button-primary">افزودن محصول جدید</a>
+    <table class="hesabfa-table">
+        <thead>
+            <tr>
+                <th>نام محصول</th>
+                <th>کد محصول</th>
+                <th>قیمت</th>
+                <th>موجودی</th>
+                <th>عملیات</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- لیست محصولات از دیتابیس نمایش داده می‌شود -->
+        </tbody>
+    </table>
+</div>
