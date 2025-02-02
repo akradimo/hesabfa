d2945b8a784d1f37f8ae793089aa9dc12401348f
akradimo <tepars.ir@gmail.com>
2025-02-03 00:31:00 +0330

    Auto-deploy changes

--- includes/models/Service.php
+++ includes/models/Service.php
@@ -1,40 +1,32 @@
 <?php
-
 namespace Hesabfa\Models;
 
-class Service
-{
+class Service {
     private $wpdb;
 
-    public function __construct()
-    {
+    public function __construct() {
         global $wpdb;
         $this->wpdb = $wpdb;
         $this->table_name = $wpdb->prefix . 'hesabfa_services';
     }
 
-    public function getAll()
-    {
+    public function getAll() {
         return $this->wpdb->get_results("SELECT * FROM {$this->table_name}");
     }
 
-    public function getById($id)
-    {
+    public function getById($id) {
         return $this->wpdb->get_row($this->wpdb->prepare("SELECT * FROM {$this->table_name} WHERE id = %d", $id));
     }
 
-    public function add($data)
-    {
+    public function add($data) {
         return $this->wpdb->insert($this->table_name, $data);
     }
 
-    public function update($id, $data)
-    {
+    public function update($id, $data) {
         return $this->wpdb->update($this->table_name, $data, array('id' => $id));
     }
 
-    public function delete($id)
-    {
+    public function delete($id) {
         return $this->wpdb->delete($this->table_name, array('id' => $id));
     }
 }
