20d0dff47294e61127975d27ab17946bbc4cd6d4
akradimo <tepars.ir@gmail.com>
2025-03-08 17:16:12 +0330

    03

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>حسابفا</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        body {
+            font-family: 'Vazirmatn', sans-serif;
+        }
+
+        .sidebar {
+            width: 250px;
+            position: fixed;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #f8f9fa;
+            padding: 20px;
+            border-left: 1px solid #dee2e6;
+        }
+
+        .content {
+            margin-right: 250px;
+            padding: 20px;
+        }
+        
+        .list-unstyled li a {
+            display: block;
+            padding: 8px 16px;
+            text-decoration: none;
+            color: #343a40;
+        }
+
+        .list-unstyled li a:hover {
+            background-color: #e9ecef;
+        }
+
+        .dropdown-toggle::after {
+            display: inline-block;
+            margin-right: 0.255em;
+            vertical-align: 0.255em;
+            content: "";
+            border-top: 0.3em solid;
+            border-right: 0.3em solid transparent;
+            border-bottom: 0;
+            border-left: 0.3em solid transparent;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebar" class="sidebar">
+                @include('layouts.sidebar')
+            </nav>
+
+            <main class="content">
+                @yield('content')
+            </main>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,281 @@
+<div class="sidebar">
+    <ul class="list-unstyled">
+        <li>
+            <a href="#">
+                <i class="fas fa-tachometer-alt"></i>
+                داشبورد
+            </a>
+        </li>
+        <li>
+            <a href="#peopleSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-users"></i>
+                اشخاص
+            </a>
+            <ul class="collapse list-unstyled" id="peopleSubmenu">
+                <li>
+                    <a href="#">شخص جدید</a>
+                </li>
+                <li>
+                    <a href="#">اشخاص</a>
+                </li>
+                <li>
+                    <a href="#">دریافت</a>
+                </li>
+                <li>
+                    <a href="#">لیست دریافت‌ها</a>
+                </li>
+                <li>
+                    <a href="#">پرداخت</a>
+                </li>
+                <li>
+                    <a href="#">لیست پرداخت‌ها</a>
+                </li>
+                <li>
+                    <a href="#">سهامداران</a>
+                </li>
+                <li>
+                    <a href="#">فروشندگان</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#productsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-box"></i>
+                کالاها و خدمات
+            </a>
+            <ul class="collapse list-unstyled" id="productsSubmenu">
+                <li>
+                    <a href="#">محصول جدید</a>
+                </li>
+                <li>
+                    <a href="#">فهرست محصولات</a>
+                </li>
+                <li>
+                    <a href="#">خدمات جدید</a>
+                </li>
+                <li>
+                    <a href="#">فهرست خدمات</a>
+                </li>
+                <li>
+                    <a href="#">به‌روزرسانی قیمت محصولات</a>
+                </li>
+                <li>
+                    <a href="#">به‌روزرسانی قیمت خدمات</a>
+                </li>
+                <li>
+                    <a href="#">چاپ بارکد</a>
+                </li>
+                <li>
+                    <a href="#">چاپ بارکد تعدادی</a>
+                </li>
+                <li>
+                    <a href="#">صفحه لیست قیمت کالا</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#bankingSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-university"></i>
+                بانکداری
+            </a>
+            <ul class="collapse list-unstyled" id="bankingSubmenu">
+                <li>
+                    <a href="#">بانک‌ها</a>
+                </li>
+                <li>
+                    <a href="#">صندوق‌ها</a>
+                </li>
+                <li>
+                    <a href="#">تنخواه‌گردان‌ها</a>
+                </li>
+                <li>
+                    <a href="#">انتقال</a>
+                </li>
+                <li>
+                    <a href="#">لیست انتقال‌ها</a>
+                </li>
+                <li>
+                    <a href="#">لیست چک‌های دریافتی</a>
+                </li>
+                <li>
+                    <a href="#">لیست چک‌های پرداختی</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#salesSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-chart-line"></i>
+                فروش و درآمد
+            </a>
+            <ul class="collapse list-unstyled" id="salesSubmenu">
+                <li>
+                    <a href="#">فروش جدید</a>
+                </li>
+                <li>
+                    <a href="#">فاکتور سریع</a>
+                </li>
+                <li>
+                    <a href="#">برگشت از فروش</a>
+                </li>
+                <li>
+                    <a href="#">فاکتورهای فروش</a>
+                </li>
+                <li>
+                    <a href="#">فاکتورهای برگشت از فروش</a>
+                </li>
+                <li>
+                    <a href="#">درآمد</a>
+                </li>
+                <li>
+                    <a href="#">لیست درآمدها</a>
+                </li>
+                <li>
+                    <a href="#">قرارداد فروش اقساطی</a>
+                </li>
+                <li>
+                    <a href="#">لیست فروش اقساطی</a>
+                </li>
+                <li>
+                    <a href="#">اقلام تخفیف‌دار</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#purchaseSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-shopping-cart"></i>
+                خرید و هزینه
+            </a>
+            <ul class="collapse list-unstyled" id="purchaseSubmenu">
+                <li>
+                    <a href="#">خرید جدید</a>
+                </li>
+                <li>
+                    <a href="#">برگشت از خرید</a>
+                </li>
+                <li>
+                    <a href="#">فاکتورهای خرید</a>
+                </li>
+                <li>
+                    <a href="#">فاکتورهای برگشت از خرید</a>
+                </li>
+                <li>
+                    <a href="#">هزینه</a>
+                </li>
+                <li>
+                    <a href="#">لیست هزینه‌ها</a>
+                </li>
+                <li>
+                    <a href="#">ضایعات</a>
+                </li>
+                <li>
+                    <a href="#">لیست ضایعات</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#inventorySubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-warehouse"></i>
+                انبارداری
+            </a>
+            <ul class="collapse list-unstyled" id="inventorySubmenu">
+                <li>
+                    <a href="#">انبارها</a>
+                </li>
+                <li>
+                    <a href="#">حواله جدید</a>
+                </li>
+                <li>
+                    <a href="#">رسید و حواله‌های انبار</a>
+                </li>
+                <li>
+                    <a href="#">موجودی کالا</a>
+                </li>
+                <li>
+                    <a href="#">موجودی تمامی انبارها</a>
+                </li>
+                <li>
+                    <a href="#">انبارگردانی</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#accountingSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-calculator"></i>
+                حسابداری
+            </a>
+            <ul class="collapse list-unstyled" id="accountingSubmenu">
+                <li>
+                    <a href="#">سند جدید</a>
+                </li>
+                <li>
+                    <a href="#">لیست اسناد</a>
+                </li>
+                <li>
+                    <a href="#">تراز افتتاحیه</a>
+                </li>
+                <li>
+                    <a href="#">بستن سال مالی</a>
+                </li>
+                <li>
+                    <a href="#">جدول حساب‌ها</a>
+                </li>
+                <li>
+                    <a href="#">تجمیع اسناد</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#otherSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-ellipsis-h"></i>
+                سایر
+            </a>
+            <ul class="collapse list-unstyled" id="otherSubmenu">
+                <li>
+                    <a href="#">آرشیو</a>
+                </li>
+                <li>
+                    <a href="#">پنل پیامک</a>
+                </li>
+                <li>
+                    <a href="#">استعلام</a>
+                </li>
+                <li>
+                    <a href="#">دریافت سایر</a>
+                </li>
+                <li>
+                    <a href="#">لیست دریافت‌ها</a>
+                </li>
+                <li>
+                    <a href="#">پرداخت سایر</a>
+                </li>
+                <li>
+                    <a href="#">لیست پرداخت‌ها</a>
+                </li>
+                <li>
+                    <a href="#">سند تسعیر ارز</a>
+                </li>
+                <li>
+                    <a href="#">سند توازن اشخاص</a>
+                </li>
+                <li>
+                    <a href="#">سند توازن کالاها</a>
+                </li>
+                <li>
+                    <a href="#">سند حقوق</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fas fa-chart-pie"></i>
+                گزارش‌ها
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fas fa-cog"></i>
+                تنظیمات
+            </a>
+        </li>
+    </ul>
+</div>
